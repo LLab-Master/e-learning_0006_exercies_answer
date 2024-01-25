57be29d7ad858a68af624039dba6951ee8c474a9
ota <ota@lighthouselab.co.jp>
2024-01-25 13:41:25 +0900

    ソースコード追加 ・追加の演習問題の解答例 ・テキストのサンプルコードの一部を記載 ・JavaScriptのサンプルコード

---
+++ src/text_sample/9/alert_sample.js
@@ -0,0 +1,8 @@
+// ボタン要素を取得
+var myButton = document.getElementById('myButton');
+
+// ボタンがクリックされたときの処理を設定
+myButton.addEventListener('click', function() {
+  // アラートを表示
+  alert('ボタンがクリックされました！');
+});
